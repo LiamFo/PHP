9a837b70242d83636d62345fac52849e82534cae
LiamFo <puppylilego@gmail.com>
2022-04-20 13:34:19 +0200

    h05 af

---
+++ H05/resultaat3.php
@@ -0,0 +1,8 @@
+<?php
+
+if($_POST["wachtwoord"] == "steveiscool" && $_POST["email"] == "steve@gmail.com"){
+        echo "<h1>Welkom</h1>";
+    }else{
+        echo "<h1>Sorry! Geen toegang</h1>";
+    }
+    
